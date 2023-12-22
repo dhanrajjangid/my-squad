8cd4d9e6bbb81d876ccbe0ba020cccedc28c287f
dhanrajjangid22 <dhanraj.jangid@ignatiuz.com>
2023-12-22 11:57:06 +0530

    teams schema updated

--- src/models/teamModel.js
+++ src/models/teamModel.js
@@ -1,24 +1,16 @@
 const mongoose = require("mongoose");
 
-const menuItemSchema = new mongoose.Schema({
+const playerSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
-  taste_rating: { type: Number, required: true },
-  price_rating: { type: Number, required: true },
-  cleanliness_rating: { type: Number, required: true },
+  position: { type: String, required: true },
 });
 
-const hotelSchema = new mongoose.Schema({
+const footballTeamSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  rating: { type: Number, required: true },
-  cleanliness: { type: Number, required: true },
-  taste: { type: Number, required: true },
-  service: { type: Number, required: true },
-  type: { type: String, required: true },
-  menu: [menuItemSchema],
-  status: { type: String, required: true },
+  overall_rating: { type: Number, required: true },
+  players: [playerSchema],
 });
 
-const Hotel = mongoose.model("Hotel", hotelSchema);
+const FootballTeam = mongoose.model("FootballTeam", footballTeamSchema);
 
-module.exports = Hotel;
+module.exports = FootballTeam;
